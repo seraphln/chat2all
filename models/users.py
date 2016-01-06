2ac4be543661317726568d017c9868409066f78b
wubin09 <wubin09@baidu.com>
2016-01-06 18:58:23 +0800

    add models module

---
+++ models/users.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf8
+#
+
+
+"""
+chat2all user model
+use MongoEngine as the backend
+"""
+
+
+from mongoengine import Document
+
+
+class User(Docuemnt):
+    """
+    User model
+    defined the basic columns
+    """
