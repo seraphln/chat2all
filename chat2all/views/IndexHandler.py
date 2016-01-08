d411413b9819f943a9e272ae94736a4b4509d019
wubin09 <wubin09@baidu.com>
2016-01-08 18:52:47 +0800

    add a redirect for test qq login

--- chat2all/views/IndexHandler.py
+++ chat2all/views/IndexHandler.py
@@ -3,8 +3,14 @@
 
 import tornado.web
 
+from sso.qq.api import APIClient as QQAPIClient
+
 
 class IndexHandler(tornado.web.RequestHandler):
     ''' '''
     def get(self):
-        self.render('templates/index.html')
+        api = QQAPIClient(config.get('qq_apiid'),
+                          config.get('qq_appkey'),
+                          redirect_uri=config.get('qq_callback_url'))
+        auth_url = api.get_authorization_url("authorize")
+        self.render('templates/index.html', auth_url=auth_url)
